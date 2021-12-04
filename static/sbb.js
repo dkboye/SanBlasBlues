213f29d45f48160bad25bf954b97d9158432937a
dkboye <64322015+dkboye@users.noreply.github.com>
2021-12-04 12:03:27 +0100

    notes and artist update

--- static/sbb.js
+++ static/sbb.js
@@ -1,45 +1,45 @@
-function updatecomment(){
+function updatecomment() {
 // Check if there is already a value in local storage:
   if (localStorage.getItem(`store`)) {
 // If so, update comment:
   document.getElementById(`comments`).innerHTML = localStorage.getItem(`store`)}
-  }
-
-function edit_comments() {
-  // get text from textarea (id = "editpost_{{post.id}}"):
-  var text = document.getElementById(`text`).value;
-  var textarea = document.getElementById(`text`);
-  var comments = document.getElementById(`comments`);
-  // save "text" to local storage:
-  localStorage.setItem(`store`, text)
-  // edit post:
-  document.getElementById(`comments`).innerHTML = localStorage.getItem(`store`)
-  textarea.style.display = 'none';
-  comments.style.display = 'block';
-  }
-
+}
+  
 function toggleEdit() {
 // get the textarea
-  var textarea = document.getElementById(`text`);
-  var comments = document.getElementById(`comments`);
+var textarea = document.getElementById(`text`);
+var comments = document.getElementById(`comments`);
 
-  // get the content of the textarea
-  var displaySetting = textarea.style.display;
+// get the content of the textarea
+var displaySetting = textarea.style.display;
 
-  // toggle the button/textarea depending on current state
-  if (displaySetting == 'block') {
+// toggle the button/textarea depending on current state
+if (displaySetting == 'block') {
   // textarea is visible -> hide it
-    textarea.style.display = 'none';
-    comments.style.display = 'block';
-  }
-  else {
+  textarea.style.display = 'none';
+  comments.style.display = 'block';
+}
+else {
   // textarea is hidden. show it
-    if (localStorage.getItem(`store`)) {
-      textarea.value = localStorage.getItem(`store`)}
+  if (localStorage.getItem(`store`)) {
+    textarea.value = localStorage.getItem(`store`)}
     else {
       textarea.value = document.getElementById(`text`).innerHTML
     }
     textarea.style.display = 'block';
     comments.style.display = 'none';
-    }
   }
+}
+
+function edit_comments() {
+// get text from textarea (id = "editpost_{{post.id}}"):
+var text = document.getElementById(`text`).value;
+var textarea = document.getElementById(`text`);
+var comments = document.getElementById(`comments`);
+// save "text" to local storage:
+localStorage.setItem(`store`, text)
+// edit post:
+document.getElementById(`comments`).innerHTML = localStorage.getItem(`store`)
+textarea.style.display = 'none';
+comments.style.display = 'block';
+}
